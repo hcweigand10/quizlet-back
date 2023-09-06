eb066e8dc03d14253223e68ca36a67b05ab6ee3a
hcweigand10 <henryweigand10@gmail.com>
2023-09-06 13:50:11 -0700

    procfile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 // Import the ApolloServer class
 const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
 const { expressMiddleware } = require('@apollo/server/express4');
 // const { authMiddleware } = require('./utils/auth');
 
@@ -10,12 +11,14 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+// const httpServer = http.createServer(app);
 
-const app = express();
 
 
 // Create a new instance of an Apollo server with the GraphQL schema
@@ -38,13 +41,6 @@ const startApolloServer = async () => {
   
   app.use('/graphql', expressMiddleware(server));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
 
   db.once('open', () => {
     app.listen(PORT, () => {
